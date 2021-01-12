5930090aa2d59333513760f21bd338a81f7b77ba
Aaron 110518 <110518@eesd.org>
2021-01-12 00:00:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hello World")
+# This print statement prints out 'Hello World'
+
+# Write 5 print statements
+print('My name is Aaron Ysunza')
+print('Im in 7th grade')
+print('My favorite color is blue')
+print('My favorite sport is swimming')
+print('My favorite ice cream flavor is vanilla')
